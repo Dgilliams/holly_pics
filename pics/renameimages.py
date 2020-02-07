d2597fb69b648ecc58b6d0691e952dd5dc945ae3
dgilliams <damosdesigns@gmail.com>
2020-02-06 21:26:14 -0500

    pics

--- pics/renameimages.py
+++ pics/renameimages.py
@@ -8,7 +8,7 @@ import os
 def main():
     i = 0
 
-    for filename in os.listdir("D:/holly/celebration/pics2/pics"):
+    for filename in os.listdir("D:/holly/celebration/pics/pics"):
         newName ="holly_celebration_" + str(i) + ".jpg"
         src ='D:/holly/celebration/pics/pics/'+ filename
         dest ='D:/holly/celebration/pics/renamedresult/'+ newName
@@ -16,7 +16,8 @@ def main():
         # rename all the files
         os.rename(src, dest)
         i += 1
-        print(newName)
+        newLoc = '\'https://github.com/Dgilliams/holly_pics/blob/master/pics/renamedresult/'+ newName +'?raw=true\','
+        print(newLoc)
 
 # Driver Code
 if __name__ == '__main__':
